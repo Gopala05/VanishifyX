48fa650b07711e582f07e23ead77fec6deab7935
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-12-04 16:26:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Nav from "@/components/custom/nav";
 
 export default function Home() {
   return (
-    <div className=""></div>
+    <div className="flex h-full">
+      <Nav />
+    </div>
   );
 }
