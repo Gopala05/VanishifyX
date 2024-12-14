d3d77fdff363d556521a9c3bb6a15b0497a01e81
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-12-14 23:06:43 +0530

    Update nav.tsx

--- components/custom/nav.tsx
+++ components/custom/nav.tsx
@@ -1,10 +1,12 @@
+// Library Imports
 import Image from "next/image";
+import Link from "next/link";
 
 const Nav = () => {
   return (
-    <header className="bg-[#070708]">
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-        <a className="block text-teal-600" href="/">
+    <header className="bg-bgcolor fixed top-0 left-0 right-0 z-10">
+      <div className="mx-auto flex h-28 max-w-screen-2xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+        <Link className="block text-teal-600" href="/">
           <Image
             src="/Logo.png"
             alt="Logo"
@@ -12,62 +14,17 @@ const Nav = () => {
             height={60}
             className="rounded-full"
           />
-        </a>
+        </Link>
 
         <div className="flex flex-1 items-center justify-end md:justify-between">
           <nav aria-label="Global" className="hidden md:block">
-            <ul className="flex items-center gap-6 text-sm">
+            <ul className="flex items-center gap-6 text-lg">
               <li>
                 <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                  className="text-white transition hover:text-teal-400 dark:text-white dark:hover:text-teal-400"
                   href="#"
                 >
                   About
-                </a>
-              </li>
-
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Careers
-                </a>
-              </li>
-
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  History
-                </a>
-              </li>
-
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Services
-                </a>
-              </li>
-
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Projects
-                </a>
-              </li>
-
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Blog
                 </a>
               </li>
             </ul>
