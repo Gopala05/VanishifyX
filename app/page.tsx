fd520fb5fbbba02df638f53dd118d05655addac1
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-12-31 22:17:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+// Library Imports
 import Nav from "@/components/custom/Nav";
 import Home from "@/components/custom/Home";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useEffect } from "react";
 
 const LandingPageView = () => {
-  return (
+  const { user } = useKindeBrowserClient();
+
+  useEffect(() => {
+    console.log(user);
+  });
+
+  return (  
     <>
       <Nav />
       <Home />
