5568de80e3507d46d7df42d2e51e2066193e7401
Gopala05 <gopigopala05vkbrg@gmail.com>
2025-01-04 23:30:54 +0530

    Update nav.tsx

--- components/custom/nav.tsx
+++ components/custom/nav.tsx
@@ -6,11 +6,21 @@ import {
   LoginLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 
-const Nav = () => {
+// Custom Imports
+import { cn } from "@/lib/utils";
+
+interface NavProps {
+  show: boolean;
+  className?: string;
+}
+
+const Nav: React.FC<NavProps> = ({ show, className }) => {
   return (
-    <header className="bg-bgcolor fixed top-0 left-0 right-0 z-10">
+    <header
+      className={cn("bg-bgcolor fixed top-0 left-0 right-0 z-10", className)}
+    >
       <div className="mx-auto flex h-28 max-w-screen-2xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-        <Link className="block text-teal-600" href="/">
+        <Link className="flex items-center gap-x-5 text-teal-600" href="/">
           <Image
             src="/Logo.png"
             alt="Logo"
@@ -18,56 +28,59 @@ const Nav = () => {
             height={60}
             className="rounded-full"
           />
+          <span className={cn("font-bold text-3xl", className)}>VanishifyX</span>
         </Link>
 
-        <div className="flex flex-1 items-center justify-end md:justify-between">
-          <nav aria-label="Global" className="hidden md:block">
-            <ul className="flex items-center gap-6 text-lg">
-              <li>
-                <a
-                  className="text-white transition hover:text-teal-400 dark:text-white dark:hover:text-teal-400"
-                  href="#"
+        {show && (
+          <div className="flex flex-1 items-center justify-end md:justify-between">
+            <nav aria-label="Global" className="hidden md:block">
+              <ul className="flex items-center gap-6 text-lg">
+                <li>
+                  <a
+                    className="text-white transition hover:text-teal-400 dark:text-white dark:hover:text-teal-400"
+                    href="#"
+                  >
+                    About
+                  </a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <LoginLink postLoginRedirectURL="/dashboard">
+                  <span className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700 dark:hover:bg-teal-500">
+                    Login
+                  </span>
+                </LoginLink>
+
+                <RegisterLink postLoginRedirectURL="/dashboard">
+                  <span className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block dark:bg-gray-800 dark:text-white dark:hover:text-white/75">
+                    Register
+                  </span>
+                </RegisterLink>
+              </div>
+
+              <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden dark:bg-gray-800 dark:text-white dark:hover:text-white/75">
+                <span className="sr-only">Toggle menu</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
                 >
-                  About
-                </a>
-              </li>
-            </ul>
-          </nav>
-
-          <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">
-              <LoginLink postLoginRedirectURL="/dashboard">
-                <span className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700 dark:hover:bg-teal-500">
-                  Login
-                </span>
-              </LoginLink>
-
-              <RegisterLink postLoginRedirectURL="/dashboard">
-                <span className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block dark:bg-gray-800 dark:text-white dark:hover:text-white/75">
-                  Register
-                </span>
-              </RegisterLink>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
             </div>
-
-            <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden dark:bg-gray-800 dark:text-white dark:hover:text-white/75">
-              <span className="sr-only">Toggle menu</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
           </div>
-        </div>
+        )}
       </div>
     </header>
   );
