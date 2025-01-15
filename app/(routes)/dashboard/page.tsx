1fcb0df01a4658190f6a3ce3d4b27399939129b0
Gopala05 <gopigopala05vkbrg@gmail.com>
2025-01-15 12:08:18 +0530

    Update page.tsx

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,18 +1,44 @@
 "use client";
+
 // Library Imports
-import React from "react";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import React, { useEffect } from "react";
+import { useConvex, useMutation } from "convex/react";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 // Custom Imports
-import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { UserType } from "@/lib/types";
+import Header from "@/components/custom/Dashboard/Header";
+import FileListTable from "@/components/custom/Dashboard/FileListTable";
 
 const DashboardView = () => {
+  const convex = useConvex();
+  const { user }: UserType = useKindeBrowserClient();
+  const createUser = useMutation(api.user.createUser);
+
+  const checkUser = async () => {
+    const response = await convex.query(api.user.getUser, {
+      email: user?.email || "",
+    });
+
+    if (!response?.length) {
+      createUser({
+        name: user?.given_name || "",
+        email: user?.email || "",
+        image: user?.picture || "",
+      }).then((resp) => {
+        console.log(resp);
+      });
+    }
+  };
+
+  useEffect(() => {
+    user && checkUser();
+  }, [user]);
   return (
-    <div>
-      DashboardView
-      <Button>
-        <LogoutLink>LogOut</LogoutLink>
-      </Button>
+    <div className="p-8">
+      <Header />
+      <FileListTable />
     </div>
   );
 };
