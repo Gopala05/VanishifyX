8e3f5023b49db45b7bfdaaa8482b45ca142a370e
Gopala05 <gopigopala05vkbrg@gmail.com>
2025-01-17 21:04:33 +0530

    Conversion Next config from ts to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: false,
   images: {
     remotePatterns: [{ hostname: "lh3.googleusercontent.com" }],
