89c18397f695713e4ef1c5ef45d08263e98f7188
Gopala05 <gopigopala05vkbrg@gmail.com>
2025-01-20 22:38:02 +0530

    Update files.tsx

--- convex/files.tsx
+++ convex/files.tsx
@@ -29,7 +29,38 @@ export const getFiles = query({
       .filter((q) => q.eq(q.field("teamId"), args.teamId))
       .order("desc")
       .collect();
+    return response;
+  },
+});
 
+export const getFileData = query({
+  args: {
+    _id: v.id("files"),
+  },
+  handler: async (ctx, args) => {
+    const response = await ctx.db.get(args._id);
+    return response;
+  },
+});
+
+export const updateDocData = mutation({
+  args: {
+    _id: v.id("files"),
+    document: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const response = await ctx.db.patch(args._id, { document: args.document });
+    return response;
+  },
+});
+
+export const updateCanvasData = mutation({
+  args: {
+    _id: v.id("files"),
+    canvas: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const response = await ctx.db.patch(args._id, { whiteboard: args.canvas });
     return response;
   },
 });
